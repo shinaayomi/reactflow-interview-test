64e9afb4719ec97307d8eeceafbfadf696c3434e
shinaayomi <lateef718@gmail.com>
2023-10-31 15:21:09 +0100

    tabs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,10 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <Link aria-details="first" to="/">
-        Task 1
+        Tab 1
       </Link>
       <Link aria-details="first" to="/second">
-        Task 2
+        Tab 2
       </Link>
     </nav>
   );
